f3ba0b51534f8579491cb6a7a8b9e0e498a9f690
HeavyMaverick <114074204+HeavyMaverick@users.noreply.github.com>
2024-05-08 22:57:35 +0700

    up

---
+++ Messenger/app/src/main/java/com/example/messenger/CreateMessageActivity.java
@@ -0,0 +1,37 @@
+package com.example.messenger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    private EditText editTextMsg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_create_message);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        editTextMsg = findViewById(R.id.editTextMessage);
+    }
+
+    public void onClickSendMsg(View view) {
+        String msg = editTextMsg.getText().toString();
+        Intent intent = new Intent(this, ReceivedMessageActivity.class);
+        intent.putExtra("msg", msg);
+        startActivity(intent);
+    }
+}
