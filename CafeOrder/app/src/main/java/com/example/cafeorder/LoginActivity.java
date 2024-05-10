37b9899b3675fd5736828db95ab15af91a36d6c5
HeavyMaverick <114074204+HeavyMaverick@users.noreply.github.com>
2024-05-10 18:25:37 +0700

    up

--- CafeOrder/app/src/main/java/com/example/cafeorder/LoginActivity.java
+++ CafeOrder/app/src/main/java/com/example/cafeorder/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.cafeorder;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText editTextName;
+    private EditText editTextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,19 @@ public class LoginActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        editTextName = findViewById(R.id.editTextName);
+        editTextPassword = findViewById(R.id.editTextTextPassword);
+    }
+    public void onClickCreateOrder(View view){
+        String name = editTextName.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        if (!name.isEmpty() && !password.isEmpty()){
+            Intent intent = new Intent(this, CreateOrderActivity.class);
+            intent.putExtra("password", password);
+            intent.putExtra("name", name);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, R.string.toast_fill_fields, Toast.LENGTH_SHORT).show();
+        }
     }
 }
