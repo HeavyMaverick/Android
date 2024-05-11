d7c946d6bd7d61250101263bc51d8907065abe25
HeavyMaverick <114074204+HeavyMaverick@users.noreply.github.com>
2024-05-11 16:23:26 +0700

    up

--- CafeOrder/app/src/main/java/com/example/cafeorder/CreateOrderActivity.java
+++ CafeOrder/app/src/main/java/com/example/cafeorder/CreateOrderActivity.java
@@ -1,6 +1,11 @@
 package com.example.cafeorder;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class CreateOrderActivity extends AppCompatActivity {
+    private TextView textViewHello;
+    private TextView textViewAdditions;
+    private CheckBox checkBoxMilk;
+    private CheckBox checkBoxSugar;
+    private CheckBox checkBoxLemon;
+    private Spinner spinnerTea;
+    private Spinner spinnerCoffee;
+    private String name;
+    private String password;
+    private String drink = getString(R.string.tea);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +35,33 @@ public class CreateOrderActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Intent intent = getIntent();
+        if (intent.hasExtra("name") && intent.hasExtra("password")) {
+            name = intent.getStringExtra("name");
+            password = intent.getStringExtra("password");
+        } else {
+            name = getString(R.string.default_name);
+            password = getString(R.string.default_password);
+        }
+        textViewHello = findViewById(R.id.textViewGreetings);
+        String hello = String.format(getString(R.string.hello_user), textViewHello);
+        textViewHello.setText(hello);
+        textViewAdditions = findViewById(R.id.textViewAdditions);
+        checkBoxMilk = findViewById(R.id.checkBoxMilk);
+        checkBoxSugar = findViewById(R.id.checkBoxSugar);
+        checkBoxLemon = findViewById(R.id.checkBoxLemon);
+        spinnerCoffee = findViewById(R.id.spinnerCoffee);
+        spinnerTea = findViewById(R.id.spinnerTea);
+    }
+
+    public void onClickChangeDrink(View view) {
+    }
+
+    public void onClickSendOrder(View view) {
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
     }
 }
