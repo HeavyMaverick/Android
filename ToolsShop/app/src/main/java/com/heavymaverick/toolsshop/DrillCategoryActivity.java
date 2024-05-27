8972438ddc700c49ada20bdc5c7c4117eab71667
HeavyMaverick <114074204+HeavyMaverick@users.noreply.github.com>
2024-05-27 23:17:31 +0700

    Started project

---
+++ ToolsShop/app/src/main/java/com/heavymaverick/toolsshop/DrillCategoryActivity.java
@@ -0,0 +1,24 @@
+package com.heavymaverick.toolsshop;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class DrillCategoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_drill_category);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ ToolsShop/app/src/main/java/com/heavymaverick/toolsshop/MainActivity.java
@@ -0,0 +1,42 @@
+package com.heavymaverick.toolsshop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+        private ListView listViewTools;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        listViewTools = findViewById(R.id.listViewTools);
+        listViewTools.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Позиция " + position, Toast.LENGTH_SHORT).show();
+                switch (position){
+                    case 0:
+                        Intent intent = new Intent(getApplicationContext(), DrillCategoryActivity.class);
+                        startActivity(intent);
+                        break;
+                }
+            }
+        });
+    }
+}
