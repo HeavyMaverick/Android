9538b8a0c2abd29a8cd016ae2ffc3a7b6b7a223d
HeavyMaverick <114074204+HeavyMaverick@users.noreply.github.com>
2024-06-16 15:00:13 +0700

    up

---
+++ TT/app/src/main/java/com/heavymaverick/tt/MainActivity.java
@@ -0,0 +1,59 @@
+package com.heavymaverick.tt;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listViewNumbers;
+    private SeekBar seekBar;
+    private ArrayList<Integer> numbers;
+    private int max = 20;
+    private int min = 1;
+    private int count = 10;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        listViewNumbers = findViewById(R.id.listViewNumbers);
+        seekBar = findViewById(R.id.seekBar);
+        seekBar.setMax(max);
+        numbers = new ArrayList<>();
+        ArrayAdapter<Integer> arrayAdapter = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, numbers);
+        listViewNumbers.setAdapter(arrayAdapter);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                for (int i = min; i <= count; i++){
+                    numbers.add(progress*i);
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
